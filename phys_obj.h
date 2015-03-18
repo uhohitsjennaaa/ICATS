6c143d2637597025ae1339464eaacbd4355e6980
Andrew Gnott <agnott@nd.edu>
2015-03-18 16:59:11 -0400

    Added [incomplete] physical object class

---
+++ phys_obj.h
@@ -0,0 +1,17 @@
+//Andrew Gnott
+//March 18, 2015
+//A base class for an object that will have be effected by physics
+
+#ifndef PHYS_OBJ_H
+#define PHYS_OBJ_H
+
+
+class PhysicalObject {
+
+
+
+
+
+}
+
+#endif
