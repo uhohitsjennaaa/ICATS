1426e743a6acf98ab92687148b5dc732bbf37d33
Nicholas Maxwell Swift <nswift@cushing56.helios.nd.edu>
2015-04-19 13:22:34 -0400

    start of actual physObj class

--- Update/phys_obj.h
+++ Update/phys_obj.h
@@ -7,30 +7,70 @@
 #define PHYS_OBJ_H
 
 
-class PhysicalObject {
+class PhysObj{
 	public: 
-		//Constructor (height, width, start positionX, start positionY, upkey, downkey, left key, right key, bounce factor);
-		PhysicalObject(int = 10, int = 10, float = 30, float = 30, int=0, int=1, int=2, int=3, float = .5);
+		//Constructor
+		PhysObj(float = 20, float = 20);
 		
-		//Functions to get position of object
-		float getPosX(void);
-		float getPosY(void);
-
-		//Functions to take movement commands, may need to be CHAR, need to understand keyboard input
-		virtual void move(void);
-	
+		//get Functions
+		float getxPos(void);
+		float getyPos(void);
+		float getxVel(void);
+		float getyVel(void);
+		float getxAccel(void);
+		float getyAccel(void);
+		
+		void gravity(void);
 	protected: 
-		int height;
-		int width; 
 		//store keys chosen to move object
-		int upKey;
-		int downKey;
-		int rightKey;
-		int leftKey;
-		float positionX;
-		float positionY;
-		float bounceFactor; //How much speed will be retained after the object hits a wall/boundary
-
+		float xPos;
+		float yPos;
+		float xVel;
+		float maxxVel;
+		float yVel;
+		float maxyVel;
+		float xAccel;
+		float yAccel;
+		float dTime;
 };
+
+PhysObj::PhysObj(float ixPos, float iyPos){
+	xPos = ixPos;
+	yPos = iyPos;
+	xVel = 0;
+	maxxVel = 8;
+	yVel = 0;
+	maxyVel = 8;
+	xAccel = 0.5;
+	yAccel = 0.2;
+	dTime = 1;
+	return;
+}
+
+float PhysObj::getxPos(){
+	return xPos;
+}
+
+float PhysObj::getyPos(){
+	return yPos;
+}
+
+float PhysObj::getxVel(){
+	return xVel;
+}
+
+float PhysObj::getyVel(){
+	return yVel;
+}
+
+float PhysObj::getxAccel(){
+	return xAccel;
+}
+
+float PhysObj::getyAccel(){
+	return yAccel;
+}
+
+
 
 #endif
